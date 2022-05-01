276c84b04296d40f3d23ad8c07dc6e3b728f994e
nad <75531472+NadeenXo@users.noreply.github.com>
2022-05-01 23:32:39 +0200

    change to another report

--- ecommerce/src/test/java/runner/TestRunner.java
+++ ecommerce/src/test/java/runner/TestRunner.java
@@ -1,38 +1,35 @@
 package runner;
 
-//import io.cucumber.junit.Cucumber;
-//import org.junit.runner.RunWith;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class)
-//@CucumberOptions(
-//        features = "src\\main\\resources\\features",
-//        glue = "stepDefinition",
-//
-//        monochrome = true,
-//        plugin = {"pretty",
-//                "html:target/cucumber.html",
-//                "json:target/cucumber.json",
-//                "junit:target/cukes.xml",
-//                "rerun:target/rerun.txt"},
-//        dryRun = false,
-//        tags = "@regression"
-//)
+@CucumberOptions(
+        features = "src\\main\\resources\\features",
+        glue = "stepDefinition",
+
+        monochrome = true,
+        plugin = {"pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "junit:target/cukes.xml",
+                "rerun:target/rerun.txt"},
+        dryRun = false,
+        tags = "@regression"
+)
 
 /**
  *
  */
-@CucumberOptions(
-        plugin = {"pretty",
-                "html:target/cucumber-reports/cucumber.html",
-                "json:target/cucumber-reports/cucumber.json"
-        },
-        features = {"src\\main\\resources\\features"},
-        glue = {"stepDefinition"},
-        tags = "@regression"
-)
+//@CucumberOptions(
+//        plugin = {"pretty",
+//                "html:target/cucumber-reports/cucumber.html",
+//                "json:target/cucumber-reports/cucumber.json"
+//        },
+//        features = {"src\\main\\resources\\features"},
+//        glue = {"stepDefinition"},
+//        tags = "@regression"
+//)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
